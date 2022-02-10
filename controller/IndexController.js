3dc49913320b42099c1cda108a6a7d10e0cd1592
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2022-02-10 20:52:42 -0300

    Controller index

--- controller/IndexController.js
+++ controller/IndexController.js
@@ -1,7 +1,7 @@
 const servicos =  require('../model/servico.json')
 const IndexController ={
   index: (req, res)=>{
-    const listaServico = JSON.stringify(servicos)
+    
     res.render('index', {listaServicos: servicos});
   }
 }
