7d92b9b5753e65cde95656e532f0355722101131
Felipe Rodrigues da Silva <1felipersilva1@gmail.com>
2022-02-13 15:58:26 -0300

    comit

--- controller/PetsController.js
+++ controller/PetsController.js
@@ -3,7 +3,7 @@ const pet = require('../model/pets.json')
 const PetsController = {
   index:(req, res) => {
    
-    res.render('index',{ title: 'o pets estão no console do servidor'}, console.log(pet[1]))
+    res.render({ title: 'o pets estão no console do servidor'}, console.log(pet[1]))
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const UserController = require('../controller/UserController')
 
 /* GET users listing. */
-router.get('/',UserController.index);
+router.get('/cadastro',UserController.cadastro);
+router.get('/login',UserController.login);
+router.get('/sobre',UserController.sobre);
 
 module.exports = router;
